6bb0cd4ccf8909fe2abe9f99d533b41fc3e7d622
Areg Nikoghosyan <aregnikogh@gmail.com>
2020-03-05 15:38:20 +0400

    fix

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,5 +1,38 @@
-import React        from 'react';
-import ClickOutside from './ClickOutside';
+import React from 'react';
+
+class ClickOutside extends React.Component {
+
+  // static propTypes = {
+  //   onClickOutside: PropTypes.func.isRequired
+  // };
+
+  container;
+
+  getContainer = (ref) => {
+    this.container = ref;
+  }
+
+  handle = (e) => {
+    const { onClickOutside } = this.props;
+    const CONTAINER = this.container;
+    if (!CONTAINER.contains(e.target)) { onClickOutside(e); }
+  }
+
+  render() {
+    // eslint-disable-next-line
+    const { children, onClickOutside, ...props } = this.props;
+    return <div className="S-click-outside" {...props} ref={this.getContainer}>{children}</div>;
+  }
+
+  componentDidMount() {
+    document.addEventListener('click', this.handle, true);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('click', this.handle, true);
+  }
+}
+
 
 class DropDown extends React.Component {
 
