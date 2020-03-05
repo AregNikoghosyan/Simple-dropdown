9b6f1843a44459ad4bcf96c429dd050407a9de83
Areg Nikoghosyan <aregnikogh@gmail.com>
2020-03-05 19:10:54 +0400

    test

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,66 @@
-var path = require('path');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
+// var path = require('path');
+// var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+// module.exports = {
+//     mode: 'production',
+//     entry: './src/components/Dropdown.jsx',
+//     output: {
+//         path: path.resolve('lib'),
+//         filename: 'Dropdown.js',
+//         libraryTarget: 'commonjs2'
+//     },
+//     plugins:[
+//         new ExtractTextPlugin({
+//             filename: 'dropdown.css',
+//         }),
+//     ],
+//     module: {
+//         rules: [
+//             {
+//                 test: /\.jsx?$/,
+//                 exclude: /(node_modules)/,
+//                 use: 'babel-loader'
+//             }, {
+//                 test: /\.*css$/,
+//                 use : ExtractTextPlugin.extract({
+//                     fallback : 'style-loader',
+//                     use : [
+//                         'css-loader',
+//                         'sass-loader'
+//                     ]
+//                 }),
+//                },
+//         ]
+//     }
+// }
+
+const HtmlPlugin = require('html-webpack-plugin')
+const CleanPlugin = require('clean-webpack-plugin')
+const MiniCssPlugin = require('mini-css-extract-plugin')
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
 module.exports = {
     mode: 'production',
-    entry: './src/components/Dropdown.jsx',
-    output: {
-        path: path.resolve('lib'),
-        filename: 'Dropdown.js',
-        libraryTarget: 'commonjs2'
-    },
-    plugins:[
-        new ExtractTextPlugin({
-            filename: 'dropdown.css',
-        }),
-    ],
+    entry: ['./src/components/Dropdown.jsx', './src/assets/styles/dropdown.css'],
     module: {
         rules: [
             {
                 test: /\.jsx?$/,
                 exclude: /(node_modules)/,
                 use: 'babel-loader'
-            }, {
-                test: /\.*css$/,
-                use : ExtractTextPlugin.extract({
-                    fallback : 'style-loader',
-                    use : [
-                        'css-loader',
-                        'sass-loader'
-                    ]
-                }),
-               },
-        ]
-    }
+            },
+            {
+                test: /\.scss$/,
+                //use: ['style-loader', 'css-loader', 'sass-loader']
+                use: [MiniCssPlugin.loader, 'css-loader', 'sass-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new CleanPlugin('dist'),
+        new HtmlPlugin(),
+        new MiniCssPlugin(),
+        new VueLoaderPlugin()
+    ]
 }
 
