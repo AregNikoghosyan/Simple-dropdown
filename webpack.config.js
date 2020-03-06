6687139c7ee34a43cb3a1bce838ff07fdb189dc1
Areg Nikoghosyan <aregnikogh@gmail.com>
2020-03-06 16:18:31 +0400

    test

--- webpack.config.js
+++ webpack.config.js
@@ -1,62 +1,36 @@
-// var path = require('path');
-// var ExtractTextPlugin = require('extract-text-webpack-plugin');
-
-// module.exports = {
-//     mode: 'production',
-//     entry: './src/components/Dropdown.jsx',
-//     output: {
-//         path: path.resolve('lib'),
-//         filename: 'Dropdown.js',
-//         libraryTarget: 'commonjs2'
-//     },
-//     plugins:[
-//         new ExtractTextPlugin({
-//             filename: 'dropdown.css',
-//         }),
-//     ],
-//     module: {
-//         rules: [
-//             {
-//                 test: /\.jsx?$/,
-//                 exclude: /(node_modules)/,
-//                 use: 'babel-loader'
-//             }, {
-//                 test: /\.*css$/,
-//                 use : ExtractTextPlugin.extract({
-//                     fallback : 'style-loader',
-//                     use : [
-//                         'css-loader',
-//                         'sass-loader'
-//                     ]
-//                 }),
-//                },
-//         ]
-//     }
-// }
-
-const HtmlPlugin = require('html-webpack-plugin')
-const MiniCssPlugin = require('mini-css-extract-plugin')
+var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     mode: 'production',
-    entry: ['./src/components/Dropdown.jsx', './src/assets/styles/dropdown.css'],
+    entry: './src/components/Dropdown.jsx',
+    output: {
+        path: path.resolve('lib'),
+        filename: 'Dropdown.js',
+        libraryTarget: 'commonjs2'
+    },
+    plugins:[
+        new ExtractTextPlugin({
+            filename: 'dropdown.css',
+        }),
+    ],
     module: {
         rules: [
             {
                 test: /\.jsx?$/,
                 exclude: /(node_modules)/,
                 use: 'babel-loader'
-            },
-            {
-                test: /\.css$/,
-                //use: ['style-loader', 'css-loader', 'sass-loader']
-                use: [MiniCssPlugin.loader, 'css-loader', 'sass-loader'],
-            },
-        ],
-    },
-    plugins: [
-        new HtmlPlugin(),
-        new MiniCssPlugin(),
-    ]
+            }, {
+                test: /\.*css$/,
+                use : ExtractTextPlugin.extract({
+                    fallback : 'style-loader',
+                    use : [
+                        'css-loader',
+                        'sass-loader'
+                    ]
+                }),
+               },
+        ]
+    }
 }
 
