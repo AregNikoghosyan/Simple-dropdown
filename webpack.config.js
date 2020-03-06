9b598e15e1ababa9ae3569b3ca05732c243f5e5e
Areg Nikoghosyan <aregnikogh@gmail.com>
2020-03-06 16:57:47 +0400

    test

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     },
     plugins: [
         new MiniCssExtractPlugin({
-            filename: 'style.css',
+            filename: 'dropdown.css',
         })
     ],
     module: {
