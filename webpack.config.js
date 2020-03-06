7485be2e586e5b8e4123707df69cad06d1eb73ca
Areg Nikoghosyan <aregnikogh@gmail.com>
2020-03-06 16:48:01 +0400

    test

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     },
     plugins: [
         new ExtractTextPlugin({
-            filename: 'main.css',
+            filename: './src/assets/styles/dropdown.css',
         }),
     ],
     module: {
