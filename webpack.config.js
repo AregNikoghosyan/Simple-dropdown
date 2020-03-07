4ea3532b22c31838d19112e057904ede702ea892
Areg Nikoghosyan <aregnikogh@gmail.com>
2020-03-07 15:05:54 +0400

    test

--- webpack.config.js
+++ webpack.config.js
@@ -1,78 +1,30 @@
-// var path = require('path');
-// var ExtractTextPlugin = require('extract-text-webpack-plugin');
-// var MiniCssExtractPlugin = require("mini-css-extract-plugin");
-// module.exports = {
-//     mode: 'production',
-//     entry: './src/components/Dropdown.jsx',
-//     output: {
-//         path: path.resolve('lib'),
-//         filename: 'Dropdown.js',
-//         libraryTarget: 'commonjs2'
-//     },
-//     plugins: [
-//         new MiniCssExtractPlugin({
-//             filename: 'main.css',
-//         })
-//     ],
-//     module: {
-//         rules: [
-//             {
-//                 test: /\.jsx?$/,
-//                 exclude: /(node_modules)/,
-//                 use: 'babel-loader'
-//             }, {
-//                 test: /\.css$/,
-//                 use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader']
-//             }
-//         ]
-//     }
-// }
-
-// webpack v4
-const path = require('path');
-// update 23.12.2018
-const nodeExternals = require("webpack-node-externals"); 
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const WebpackMd5Hash = require("webpack-md5-hash");
+var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
     mode: 'production',
-    entry: { main: './src/components/Dropdown.jsx' },
+    entry: './src/components/Dropdown.jsx',
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve('lib'),
         filename: 'Dropdown.js',
+        libraryTarget: 'commonjs2'
     },
-    target: "node",
-    externals: [nodeExternals()], module: {
+    plugins: [
+        new ExtractTextPlugin({
+            filename: 'main.css',
+        })
+    ],
+    module: {
         rules: [
             {
-                test: /\.jsx$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
+                test: /\.jsx?$/,
+                exclude: /(node_modules)/,
+                use: 'babel-loader'
             },
             {
                 test: /\.css$/,
-                use: ['style-loader',
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'postcss-loader',
-                    'sass-loader']
+                use: ['style-loader', 'css-loader']
             }
         ]
-    },
-    plugins: [
-        // new CleanWebpackPlugin('dist', {}),
-        new MiniCssExtractPlugin({
-            filename: 'main.css',
-        }),
-        new HtmlWebpackPlugin({
-            inject: false,
-            hash: true,
-            template: './dist/index.html',
-            filename: 'index.html'
-        }),
-        new WebpackMd5Hash()
-    ]
-};
+    }
+}
