87ec24cacae7846b59e6f5ddd942ad5c31c8ac54
Areg Nikoghosyan <aregnikogh@gmail.com>
2020-03-10 14:38:37 +0400

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,12 @@
 import React, { Component } from 'react';
-import DropDown from './components/Dropdown';
 
 class App extends Component {
-
-  componentDidMount() {
-
-  }
-
-  list = [
-    {
-      name: 'test',
-      value: 1,
-    },
-    {
-      name: 'asd',
-      value: 2,
-    },
-    {
-      name: 'qwe',
-      value: 3,
-    }
-  ]
-
-  change = value => this.setState({ selectedCategory: value });
-
   render() {
 
     return (
       <div>
-        <DropDown placeholder="Choose" list={this.list} onSelect={this.change}></DropDown>
-      </div>
+
+      </div> 
     );
   }
 }
